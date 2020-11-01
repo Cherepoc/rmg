a82875a74b105ad5ab2bb2c139a1e2fa7d30aea1
aerokhina <erokhina4ar@gmail.com>
2020-11-02 00:35:32 +0300

    Added own track patterns

--- RMG.CoreF/RandomMusicGenerator.fs
+++ RMG.CoreF/RandomMusicGenerator.fs
@@ -19,11 +19,12 @@ module RandomMusicGenerator =
         let context = Generate.Context()
         let duration = 512.0
         let templateCount = 16
+        let subTemplateCount = templateCount / 2
 
         let scale: Scale =
             { KeyOffsets = [ 0; 2; 3; 5; 7; 8; 10 ] }
 
-        let tempo = context |> Generate.float (0.75, 1.5)
+        let tempo = context |> Generate.float (0.75, 1.25)
 
         let trackCount = context |> Generate.int (4, 8)
 
@@ -49,72 +50,106 @@ module RandomMusicGenerator =
                         ScaleOffset = 0
                         Volume = context |> Generate.float (0.75, 1.5) } }) trackCount
 
-        let patterns =
+        let createPattern context =
+            let duration: float =
+                2.0 ** (float (context |> Generate.int (0, 2)))
+
+            let offset =
+                context
+                |> Generate.rhythmValue (halfProbabilityFunction, 2.0, 4.0, 0.0, 4.0, 4)
+
+            let period: float =
+                (context
+                 |> Generate.rhythmPeriod (halfProbabilityFunction, 8))
+                / (2.0
+                   ** float
+                       (context
+                        |> Generate.intByRank (quarterProbabilityFunction, -1, 2)))
+
+            let maxRank = 4
+
+            let noteGenerator =
+                fun context rank ->
+                    { Duration =
+                          (context
+                           |> Generate.rhythmValue (halfProbabilityFunction, 1.0, 2.0, 0.0, 2.0, 4))
+                          / 4.0
+                      Volume =
+                          context
+                          |> Generate.rhythmValue (threeQuarterProbabilityFunction, 0.5, 1.0, 0.75, 1.5, 5)
+                      OctaveOffset =
+                          context
+                          |> Generate.intByRank (eighthProbabilityFunction, -1, 1)
+                      ScaleOffset =
+                          context
+                          |> Generate.intByRank (halfProbabilityFunction, -2, 2)
+                      KeyOffset = 0 }
+
+            let notes =
+                context
+                |> Generate.timeline noteGenerator (quarterProbabilityFunction, maxRank, offset, period, duration)
+
+            EventPattern<'T>(duration, notes, Seq.empty)
+
+        let commonPatterns =
             context
-            |> Generate.sequence (fun context ->
-                let duration: float =
-                    2.0 ** (float (context |> Generate.int (0, 2)))
-
-                let offset =
-                    context
-                    |> Generate.rhythmValue (halfProbabilityFunction, 2.0, 4.0, 0.0, 4.0, 4)
-
-                let period: float =
-                    (context
-                     |> Generate.rhythmPeriod (threeQuarterProbabilityFunction, 8))
-//                    / (2.0
-//                       ** float
-//                           (context
-//                            |> Generate.intByRank (threeQuarterProbabilityFunction, -1, 1)))
-
-                let maxRank = 4
-
-                let noteGenerator =
-                    fun context rank ->
-                        { Duration =
-                              (context
-                              |> Generate.rhythmValue (halfProbabilityFunction, 1.0, 2.0, 0.0, 2.0, 4)) / 4.0
-                          Volume =
-                              context
-                              |> Generate.rhythmValue (threeQuarterProbabilityFunction, 0.5, 1.0, 0.75, 1.5, 5)
-                          OctaveOffset =
-                              context
-                              |> Generate.intByRank (eighthProbabilityFunction, -1, 1)
-                          ScaleOffset =
-                              context
-                              |> Generate.intByRank (halfProbabilityFunction, -2, 2)
-                          KeyOffset = 0 }
-
-                let notes =
-                    context
-                    |> Generate.timeline noteGenerator (quarterProbabilityFunction, maxRank, offset, period, duration)
-
-                EventPattern<'T>(duration, notes, Seq.empty)) templateCount
+            |> Generate.sequence createPattern templateCount
             |> Seq.toArray
 
-        let higherPatterns =
+        let trackPatterns =
+            tracks
+            |> Seq.map (fun _ ->
+                seq {
+                    yield! context
+                           |> Generate.subSequence commonPatterns subTemplateCount
+                    yield! context
+                           |> Generate.sequence createPattern subTemplateCount
+                }
+                |> Seq.toArray)
+            |> Seq.indexed
+            |> Map.ofSeq
+
+        let createHigherPattern (sourcePatterns: seq<EventPattern<NoteOffset>>) context =
+            let duration =
+                2.0 ** float (context |> Generate.int (2, 4))
+
+            let limitPatternCount = context |> Generate.int (1, 4)
+
+            let limitedPatterns =
+                context
+                |> Generate.subSequence sourcePatterns limitPatternCount
+
+            let innerPatterns =
+                context
+                |> Generate.sequentialTimeline (fun context ->
+                    let item = context |> Generate.item limitedPatterns
+                    (item, item.Duration)) duration
+
+            { PatternTimelineInput = innerPatterns
+              NoteOffsetStatePatternMapPatternTimelineInput = Seq.empty
+              NoteOffsetBasedPatternTimelineInput = Seq.empty }
+            |> NoteOffsetStateBasedEventPattern.fromInput duration
+
+        let commonHigherPatterns =
             context
-            |> Generate.sequence (fun context ->
-                let duration =
-                    2.0 ** float (context |> Generate.int (2, 4))
-
-                let limitPatternCount = context |> Generate.int (1, 4)
-
-                let limitedPatterns =
-                    context
-                    |> Generate.subSequence patterns limitPatternCount
-
-                let innerPatterns =
-                    context
-                    |> Generate.sequentialTimeline (fun context ->
-                        let item = context |> Generate.item limitedPatterns
-                        (item, item.Duration)) duration
-
-                { PatternTimelineInput = innerPatterns
-                  NoteOffsetStatePatternMapPatternTimelineInput = Seq.empty
-                  NoteOffsetBasedPatternTimelineInput = Seq.empty }
-                |> NoteOffsetStateBasedEventPattern.fromInput duration) templateCount
+            |> Generate.sequence (createHigherPattern commonPatterns) templateCount
             |> Seq.toArray
+
+        let trackHigherPatterns =
+            tracks
+            |> Seq.indexed
+            |> Seq.map (fun (index, _) ->
+                let patterns =
+                    seq {
+                        yield! context
+                               |> Generate.subSequence commonHigherPatterns subTemplateCount
+                        yield! context
+                               |> Generate.sequence (createHigherPattern trackPatterns.[index]) subTemplateCount
+                    }
+                    |> Seq.toArray
+
+                (index, patterns))
+            |> Map.ofSeq
 
         let partNoteOffsetState (period, duration) =
             let generateFloatNoteOffsetTimeline (itemFunction: Generate.Context -> int -> float): Timeline<float> =
@@ -134,7 +169,7 @@ module RandomMusicGenerator =
             let noteOffsetState =
                 { DurationPatternInput =
                       { TimelineInput =
-                            generateFloatNoteOffsetTimeline (fun context rank -> context |> Generate.float (0.75, 1.5))
+                            generateFloatNoteOffsetTimeline (fun context rank -> context |> Generate.float (0.5, 2.0))
                         PatternTimelineInput = Seq.empty }
                   KeyOffsetPatternInput =
                       { TimelineInput =
@@ -154,7 +189,7 @@ module RandomMusicGenerator =
                         PatternTimelineInput = Seq.empty }
                   VolumePatternInput =
                       { TimelineInput = Seq.empty
-                            //generateFloatNoteOffsetTimeline (fun context rank -> context |> Generate.float (0.75, 1.5))
+                        //generateFloatNoteOffsetTimeline (fun context rank -> context |> Generate.float (0.75, 1.5))
                         PatternTimelineInput = Seq.empty } }
                 |> NoteOffsetStatePatternMap.fromSourceInput duration
 
@@ -166,81 +201,80 @@ module RandomMusicGenerator =
               EventPatternInput.PatternTimelineInput = Seq.empty }
             |> EventPattern.fromInput duration
 
+        let createPart context =
+            let duration =
+                2.0 ** float (context |> Generate.int (4, 6))
+
+            let partTrackCount = context |> Generate.int (1, 4)
+
+            let trackNumbers =
+                context
+                |> Generate.subSequence (seq { 0 .. trackCount - 1 }) partTrackCount
+
+            let trackTimelines =
+                trackNumbers
+                |> Seq.map (fun trackNumber ->
+                    let limitPatternCount = context |> Generate.int (2, 8)
+
+                    let limitedPatterns =
+                        context
+                        |> Generate.subSequence trackHigherPatterns.[trackNumber] limitPatternCount
+
+                    let innerPatterns =
+                        context
+                        |> Generate.sequentialTimeline (fun context ->
+                            let item = context |> Generate.item limitedPatterns
+                            (item, item.Duration)) duration
+
+                    (trackNumber, innerPatterns))
+
+            let noteOffsetStatePattern = partNoteOffsetState (16.0, duration)
+
+            { TrackPatternTimelineMapInput = trackTimelines
+              NoteOffsetStatePatternMapPatternTimelineInput =
+                  seq {
+                      { Position = 0.0
+                        Value = noteOffsetStatePattern }
+                  }
+              TrackNoteOffsetStateBasedEventPatternMapTimelineInput = Seq.empty }
+            |> TrackNoteOffsetStateBasedEventPatternMap.fromInput duration
+
         let parts =
             context
-            |> Generate.sequence (fun context ->
-                let duration =
-                    2.0 ** float (context |> Generate.int (4, 6))
-
-                let partTrackCount = context |> Generate.int (1, 4)
-
-                let trackNumbers =
-                    context
-                    |> Generate.subSequence (seq { 0 .. trackCount - 1 }) partTrackCount
-
-                let trackTimelines =
-                    trackNumbers
-                    |> Seq.map (fun trackNumber ->
-                        let limitPatternCount = context |> Generate.int (2, 8)
-
-                        let limitedPatterns =
-                            context
-                            |> Generate.subSequence higherPatterns limitPatternCount
-
-                        let innerPatterns =
-                            context
-                            |> Generate.sequentialTimeline (fun context ->
-                                let item = context |> Generate.item limitedPatterns
-                                (item, item.Duration)) duration
-
-                        (trackNumber, innerPatterns))
-
-                let noteOffsetStatePattern = partNoteOffsetState (16.0, duration)
-
-                { TrackPatternTimelineMapInput = trackTimelines
-                  NoteOffsetStatePatternMapPatternTimelineInput =
-                      seq {
-                          { Position = 0.0
-                            Value = noteOffsetStatePattern }
-                      }
-                  TrackNoteOffsetStateBasedEventPatternMapTimelineInput = Seq.empty }
-                |> TrackNoteOffsetStateBasedEventPatternMap.fromInput duration) templateCount
+            |> Generate.sequence createPart templateCount
             |> Seq.toArray
 
-        let higherParts =
-            context
-            |> Generate.sequence (fun context ->
-                let duration =
-                    2.0 ** float (context |> Generate.int (6, 8))
+        let createHigherPart context =
+            let duration =
+                2.0 ** float (context |> Generate.int (6, 8))
 
-                let limitPartCount = context |> Generate.int (1, 4)
+            let limitPartCount = context |> Generate.int (1, 4)
 
-                let limitedParts =
-                    context
-                    |> Generate.subSequence parts limitPartCount
+            let limitedParts =
+                context
+                |> Generate.subSequence parts limitPartCount
 
-                let innerParts =
-                    context
-                    |> Generate.sequentialTimeline (fun context ->
-                        let item = context |> Generate.item limitedParts
-                        (item, item.Duration)) duration
+            let innerParts =
+                context
+                |> Generate.sequentialTimeline (fun context ->
+                    let item = context |> Generate.item limitedParts
+                    (item, item.Duration)) duration
 
-                let noteOffsetStatePattern = partNoteOffsetState (16.0, duration)
+            let noteOffsetStatePattern = partNoteOffsetState (16.0, duration)
 
-                { TrackPatternTimelineMapInput = Seq.empty
-                  NoteOffsetStatePatternMapPatternTimelineInput =
-                      seq {
-                          { Position = 0.0
-                            Value = noteOffsetStatePattern }
-                      }
-                  TrackNoteOffsetStateBasedEventPatternMapTimelineInput = innerParts }
-                |> TrackNoteOffsetStateBasedEventPatternMap.fromInput duration) templateCount
-            |> Seq.toArray
+            { TrackPatternTimelineMapInput = Seq.empty
+              NoteOffsetStatePatternMapPatternTimelineInput =
+                  seq {
+                      { Position = 0.0
+                        Value = noteOffsetStatePattern }
+                  }
+              TrackNoteOffsetStateBasedEventPatternMapTimelineInput = innerParts }
+            |> TrackNoteOffsetStateBasedEventPatternMap.fromInput duration
 
         let songTimeline =
             context
             |> Generate.sequentialTimeline (fun context ->
-                let item = context |> Generate.item higherParts
+                let item = createHigherPart context
                 (item, item.Duration)) (duration)
 
         let toStatePatternTimeline value =
