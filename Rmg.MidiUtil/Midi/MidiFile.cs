6c922c806b9d4f6c7ea494d993da377524a1620c
Alexander Sharovatov <checherocka@gmail.com>
2025-07-25 00:17:10 +0400

    Remove the midi util - it has its own repository now

--- Rmg.MidiUtil/Midi/MidiFile.cs
+++
@@ -1,8 +0,0 @@
-namespace Rmg.MidiUtil.Midi;
-
-public sealed record MidiFile
-{
-    public required byte[] Data { get; init; }
-
-    public required IReadOnlyList<Track> Tracks { get; init; }
-}
--- Rmg.MidiUtil/Midi/Track.cs
+++
@@ -1,8 +0,0 @@
-namespace Rmg.MidiUtil.Midi;
-
-public sealed record Track
-{
-    public required int StartIndex { get; init; }
-    public required int Length { get; init; }
-    public required IReadOnlyList<ITrackEvent> Events { get; init; }
-}
